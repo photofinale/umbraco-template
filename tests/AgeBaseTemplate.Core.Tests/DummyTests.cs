0d917b30e15924324134f2dd5a883c95e1801156
Dan Lister <d.lister@agebase.co.uk>
2016-11-22 15:35:11 +0000

    Added dummy test class

---
+++ tests/AgeBaseTemplate.Core.Tests/DummyTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AgeBaseTemplate.Core.Tests
+{
+    [TestClass]
+    public class DummyTests
+    {
+        [TestInitialize]
+        public void Setup()
+        { }
+
+        [TestMethod]
+        public void Method_Given_Returns()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
