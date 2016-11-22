9a01a0ff04e2825dfdaced837b912bc60e0bbcc4
Dan Lister <d.lister@agebase.co.uk>
2016-11-22 14:19:17 +0000

    Added AgeBase.DomainManager package

--- readme.md
+++ readme.md
@@ -1,3 +1,11 @@
+# AgeBase Umbraco Template
+
+**AgeBase Umbraco Template** is a hopefully handy template for creating Umbraco web applications. It includes your usually Umbraco features and also some nice extra features. Things like automated deployments, strongly typed models, donut caching, gulp scripts and Azure hosting. We've also added some handy scripts to managed Umbraco databases and stting up a development machine to run the template.
+
+### Features
+
+The project is a work in progress. We have a desired feature list which shows what we've completed so far. If you'd like to add a feature suggestion, please send the repository a pull request.
+
 - Umbraco
  - [ ] Base Template
  - [ ] Country Page
@@ -17,7 +25,7 @@
  - [ ] Site Messages
  - [ ] Multiple Sites
 - Web
- - [ ] Domain Manager
+ - [x] Domain Manager
  - [x] Robots.txt
  - [ ] Favicon
  - [ ] Donut Caching
---
+++ src/AgeBaseTemplate/app_plugins/AgeBase.DomainManager/Index.Controller.js
@@ -0,0 +1,28 @@
+﻿angular.module("umbraco").controller("AgeBase.DomainManager.IndexController", function ($scope, $http, notificationsService) {
+
+    $http.get("backoffice/AgeBaseDomainManager/DomainManagerApi/Get").then(function (response) {
+        $scope.domains = response.data;
+
+        for (var i = 0; i < $scope.domains.length; i++) {
+            if ($scope.domains[i].Name.toLowerCase().indexOf("http://") != 0 && $scope.domains[i].Name.toLowerCase().indexOf("https://") != 0)
+                $scope.domains[i].Url = window.location.protocol + "//" + $scope.domains[i].Name;
+            else
+                $scope.domains[i].Url = $scope.domains[i].Name;
+        }
+    });
+
+    $scope.showNode = function (nodeId) {
+        window.location = "/umbraco/#/content/content/edit/" + nodeId;
+    };
+
+    $scope.deleteDomain = function (domain) {
+        $http.delete("backoffice/AgeBaseDomainManager/DomainManagerApi/Delete/" + domain.Id).then(function (response) {
+            if (response && response.data) {
+                $scope.domains.pop(domain);
+            } else {
+                notificationsService.error("ERROR", "It looks like the domain failed to delete correctly. Please try again.");
+            }
+        });
+    };
+
+});
