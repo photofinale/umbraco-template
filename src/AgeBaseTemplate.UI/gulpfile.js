e5eff5f6218ea481060e80b14fbd7666f8881bca
Matt Perry <matt.perry@dillorscroft.co.uk>
2016-12-14 14:31:42 +0000

    Added Gulp-Sass

--- src/AgeBaseTemplate.UI/gulpfile.js
+++ src/AgeBaseTemplate.UI/gulpfile.js
@@ -1,3 +1,12 @@
 var gulp = require("gulp");
+var $ = require('gulp-load-plugins')({ lazy: true });
 
-gulp.task('default', [], function() { });
+gulp.task('default', ['styles'], function() { });
+
+gulp.task('styles', function() {
+
+    return gulp.src('app/assets/sass/**/*.scss')
+        .pipe($.sass().on('error', $.sass.logError))
+        .pipe(gulp.dest('dist/assets/css'));
+
+});
