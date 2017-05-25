8e8eb17f19e6f04302a9ec0c3d531d1c42aa9c9f
Dan Lister <d.lister@agebase.co.uk>
2017-05-25 13:15:13 +0100

    Upgraded Umbraco to v7.6.1

--- src/AgeBaseTemplate.Database/Database.sql
+++ src/AgeBaseTemplate.Database/Database.sql
@@ -1189,7 +1189,7 @@ GO
 SET IDENTITY_INSERT [dbo].[umbracoMigration] ON 
 
 GO
-INSERT [dbo].[umbracoMigration] ([id], [name], [createDate], [version]) VALUES (11, N'Umbraco', CAST(N'2017-05-03 10:29:14.770' AS DateTime), N'7.6.0')
+INSERT [dbo].[umbracoMigration] ([id], [name], [createDate], [version]) VALUES (12, N'Umbraco', CAST(N'2017-05-25 13:06:16.600' AS DateTime), N'7.6.1')
 GO
 SET IDENTITY_INSERT [dbo].[umbracoMigration] OFF
 GO
@@ -1296,7 +1296,7 @@ GO
 SET IDENTITY_INSERT [dbo].[umbracoUser] ON 
 
 GO
-INSERT [dbo].[umbracoUser] ([id], [userDisabled], [userNoConsole], [userType], [startStructureID], [startMediaID], [userName], [userLogin], [userPassword], [userEmail], [userLanguage], [securityStampToken], [failedLoginAttempts], [lastLockoutDate], [lastPasswordChangeDate], [lastLoginDate]) VALUES (0, 0, 0, 1, -1, -1, N'Dan Lister', N'd.lister@agebase.co.uk', N'CXNnVWSN440vtoPyuQ+BBlgm3uU=', N'd.lister@agebase.co.uk', N'en-GB', N'3165769c-debc-4941-bf1a-c094b38eb15f', 0, NULL, CAST(N'2016-09-20 10:32:05.533' AS DateTime), CAST(N'2017-05-03 10:40:56.497' AS DateTime))
+INSERT [dbo].[umbracoUser] ([id], [userDisabled], [userNoConsole], [userType], [startStructureID], [startMediaID], [userName], [userLogin], [userPassword], [userEmail], [userLanguage], [securityStampToken], [failedLoginAttempts], [lastLockoutDate], [lastPasswordChangeDate], [lastLoginDate]) VALUES (0, 0, 0, 1, -1, -1, N'Dan Lister', N'd.lister@agebase.co.uk', N'CXNnVWSN440vtoPyuQ+BBlgm3uU=', N'd.lister@agebase.co.uk', N'en-GB', N'3165769c-debc-4941-bf1a-c094b38eb15f', 0, NULL, CAST(N'2016-09-20 10:32:05.533' AS DateTime), CAST(N'2017-05-25 13:05:54.720' AS DateTime))
 GO
 SET IDENTITY_INSERT [dbo].[umbracoUser] OFF
 GO
